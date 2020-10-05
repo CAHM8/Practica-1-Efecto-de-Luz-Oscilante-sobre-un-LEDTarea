d15951cffc9438d786abf776435e16e38415aaee
CAHM8 <72019404+CAHM8@users.noreply.github.com>
2020-10-05 07:48:24 -0500

    Update ledosc.s

--- Ledoscilador.X/ledosc.s
+++ Ledoscilador.X/ledosc.s
@@ -29,21 +29,20 @@ PROCESSOR 16F887
    PSECT code
     main:
     BANKSEL PORTA 
-    BANKSEL TRISA
+    clrf PORTA
+    BANKSEL TRISA       
     clrf TRISA
     loop:
-    movlw 0x00
+    movlw 0b00000000    ;se colocan todos los pines del porta en 0
     movwf PORTA
     nop
    ronda1:
-    RLF  PORTA,1
-    BTFSS PORTA,7
-    goto ronda1
+    RLF  PORTA,1         ;empieza un recorrimiento de un bit de derecha a izquierda
+    BTFSS PORTA,7	 ;cuando la instruccion BTFSS detecta el estado logico 1 en el bit 7...
+    goto ronda1		 ;salta la instruccion "goto ronda1" lo que hace que inicie "ronda 2"
    ronda2:
-    RRF PORTA,1
-    BTFSS PORTA,0
-    goto ronda2
-    goto ronda1
+    RRF PORTA,1		 ;Inicia el recorrimiento del 1 desde el bit 7 al 0
+    BTFSS PORTA,0	 ;cuando por fin ese 1 llega al bit 0 la instruccion BTFSS salta la...
+    goto ronda2		 ;linea de codigo 46 devolviendo el programa a "ronda 1"
+    goto ronda1		 ;que es donde inicia de nuevo la secuencia
     END
-
-
